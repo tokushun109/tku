286d7995060e4e842ca886a65e3d6303efbcadd0
tokushun109 <tokushun109@gmail.com>
2021-01-17 17:45:06 +0900

    #9 dbをsqliteからmysqlに変更

--- api/api/settings.py
+++ api/api/settings.py
@@ -72,8 +72,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tku',
+        'USER': 'root',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
