8291e02ea0eaaa6e0fe8780a4f59809fd846b913
tokushun109 <tokushun109@gmail.com>
2021-01-17 15:41:34 +0900

    #7 Productsモデルの作成

--- api/app/models.py
+++ api/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    '''
+    商品のモデル
+    '''
+    # 名前
+    name = models.CharField(max_length=120)
+    # 商品説明
+    description = models.TextField(max_length=3000)
+
+    def __str__(self):
+        return self.name
