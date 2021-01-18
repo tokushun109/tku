f273416679f7aeef08d0468ef2fff1b22dc08699
tokushun109 <tokushun109@gmail.com>
2021-01-18 09:42:42 +0900

    #15 modelの値をapiで返せるように修正

---
+++ api/app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductsViewSet
+
+router = DefaultRouter()
+router.register(r"product", ProductsViewSet)
+
+urlpatterns = [path("", include(router.urls))]
--- api/app/views.py
+++ api/app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+
+class ProductsViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
