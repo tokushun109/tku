f226ee8f299d2c2b51fd2c8b791dbd81e23c542b
tokushun109 <tokushun109@gmail.com>
2021-01-24 18:28:58 +0900

    #31 ProductにAccessoryCategoryのForeignKeyを作成

--- api/app/models.py
+++ api/app/models.py
@@ -4,6 +4,27 @@ from django.utils import timezone
 import uuid
 
 
+class AccessoryCategory(models.Model):
+    '''
+    アクセサリーカテゴリーモデル
+    '''
+    class Meta:
+        verbose_name_plural = 'アクセサリーカテゴリー'
+
+    # uuid
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    # 名前
+    name = models.CharField('アクセサリーカテゴリー名', max_length=120)
+    # 作成日
+    created_at = models.DateTimeField(
+        '作成日', auto_now_add=True)
+    # 更新日
+    updated_at = models.DateTimeField(
+        '更新日', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     '''
     商品のモデル
@@ -17,27 +38,9 @@ class Product(models.Model):
     name = models.CharField('商品名', max_length=120)
     # 商品説明
     description = models.TextField('商品説明')
-    # 作成日
-    created_at = models.DateTimeField(
-        '作成日', auto_now_add=True)
-    # 更新日
-    updated_at = models.DateTimeField(
-        '更新日', auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-class AccessoryCategory(models.Model):
-    '''
-    アクセサリーカテゴリーモデル
-    '''
-    class Meta:
-        verbose_name_plural = 'アクセサリーカテゴリー'
-
-    # uuid
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
-    # 名前
-    name = models.CharField('アクセサリーカテゴリー名', max_length=120)
+    # アクセサリーカテゴリー
+    accessory_category = models.ForeignKey(
+        AccessoryCategory, verbose_name='アクセサリーカテゴリー', on_delete=models.SET_NULL, null=True)
     # 作成日
     created_at = models.DateTimeField(
         '作成日', auto_now_add=True)
