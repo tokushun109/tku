11f853aacde177a0e932ad3299adf2fd551b3c08
tokushun109 <tokushun109@gmail.com>
2021-01-24 15:27:26 +0900

    #29 AccessoryCategoryモデルを作成

--- api/app/models.py
+++ api/app/models.py
@@ -16,7 +16,28 @@ class Product(models.Model):
     # 名前
     name = models.CharField('商品名', max_length=120)
     # 商品説明
-    description = models.TextField('商品説明', max_length=3000)
+    description = models.TextField('商品説明')
+    # 作成日
+    created_at = models.DateTimeField(
+        '作成日', auto_now_add=True)
+    # 更新日
+    updated_at = models.DateTimeField(
+        '更新日', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class AccessoryCategory(models.Model):
+    '''
+    アクセサリーカテゴリーモデル
+    '''
+    class Meta:
+        verbose_name_plural = 'アクセサリーカテゴリー'
+
+    # uuid
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    # 名前
+    name = models.CharField('アクセサリーカテゴリー名', max_length=120)
     # 作成日
     created_at = models.DateTimeField(
         '作成日', auto_now_add=True)
