7c194f20148aaab925a97f354248fbef9d2ad543
tokushun109 <tokushun109@gmail.com>
2021-01-30 09:10:47 +0900

    #37 material_categoryモデルを作成

--- api/app/admin.py
+++ api/app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Product, AccessoryCategory
+from .models import Product, AccessoryCategory, MaterialCategory
 
-admin_model_list = [Product, AccessoryCategory]
+admin_model_list = [Product, AccessoryCategory, MaterialCategory]
 for admin_model in admin_model_list:
     admin.site.register(admin_model)
