6d1c634052102bca24f194f96a84d2c2c89280b7
tokushun109 <tokushun109@gmail.com>
2021-01-30 17:51:48 +0900

    #41 材料カテゴリーのget(一覧)を作成する

--- api/app/urls.py
+++ api/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductListAPIView, AccessoryListAPIView, ProductRetrieveAPIView
+from .views import ProductListAPIView, ProductRetrieveAPIView, AccessoryCategoryListAPIView, MaterialCategoryListAPIView
 
 urlpatterns = [
     path('product/', ProductListAPIView.as_view()),
     path('product/<pk>/', ProductRetrieveAPIView.as_view()),
-    path('accessory_category/', AccessoryListAPIView.as_view()),
+    path('accessory_category/', AccessoryCategoryListAPIView.as_view()),
+    path('material_category/', MaterialCategoryListAPIView.as_view()),
     ]
