51a33e6e509eafb0a18356cb2fa53c4b611972e4
tokushun109 <tokushun109@gmail.com>
2021-02-06 21:03:40 +0900

    #62 仕事の依頼ページを作成

---
+++ client/pages/order/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>{{ skillMarket }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Context } from '@nuxt/types'
+import { Component, Vue } from 'nuxt-property-decorator'
+import { ISkillMarket } from '~/types'
+@Component({
+    head: {
+        title: '仕事の依頼',
+    },
+})
+export default class PageProductIndex extends Vue {
+    skillMarket: Array<ISkillMarket> | null = []
+    async asyncData({ app }: Context) {
+        try {
+            const skillMarket = await app.$axios.$get(`/skill_market/`)
+            return { skillMarket }
+        } catch (e) {
+            return { skillMarket: [] }
+        }
+    }
+}
+</script>
+
+<style lang="stylus"></style>
