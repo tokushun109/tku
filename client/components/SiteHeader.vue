5b1da548d3c71dc74f91759551c753c0aaed04d8
tokushun109 <tokushun109@gmail.com>
2021-02-09 20:35:23 +0900

    #64 headerのコンポーネントを作成

---
+++ client/components/SiteHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <div class="header-wrapper">
+            <nuxt-link class="site-title" to="/">
+                <h1>tku</h1>
+            </nuxt-link>
+            <div id="nav-drawer">
+                <li><nuxt-link to="/product">商品</nuxt-link></li>
+                <li><nuxt-link to="/producer_profile">製作者</nuxt-link></li>
+                <li><nuxt-link to="/order">仕事の依頼</nuxt-link></li>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component({})
+export default class Header extends Vue {}
+</script>
+
+<style lang="stylus"></style>
