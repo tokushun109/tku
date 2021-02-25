b1c8454a981692c8eced51258be622e93410cf9f
tokushun109 <tokushun109@gmail.com>
2021-02-25 09:27:43 +0900

    #64 簡易的にheaderのコンポーネントを作成

--- client/components/SiteHeader.vue
+++ client/components/SiteHeader.vue
@@ -1,16 +1,15 @@
 <template>
     <header>
         <div class="header-wrapper">
-            <nuxt-link class="site-title" to="/">
-                <h1>tku</h1>
-            </nuxt-link>
-            <div id="nav-drawer">
+            <div v-if="menuOpenFrag" class="open-menu">
                 <ul>
                     <li><nuxt-link to="/product">商品</nuxt-link></li>
                     <li><nuxt-link to="/producer_profile">製作者</nuxt-link></li>
                     <li><nuxt-link to="/order">仕事の依頼</nuxt-link></li>
                 </ul>
+                <img class="close-icon" src="/icon/close.png" alt="close" @click="toggle" />
             </div>
+            <img v-else class="menu-icon" src="/icon/menu.png" alt="menu" @click="toggle" />
         </div>
     </header>
 </template>
@@ -18,7 +17,13 @@
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 @Component({})
-export default class Header extends Vue {}
+export default class Header extends Vue {
+    menuOpenFrag: boolean = false
+    toggle() {
+        this.menuOpenFrag = !this.menuOpenFrag
+        return this.menuOpenFrag
+    }
+}
 </script>
 
 <style lang="stylus"></style>
