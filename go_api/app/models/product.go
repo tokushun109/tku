c69ed872171d53588dfb54734edb75e4e32aad41
tokunaga <tokunaga@limar.co.jp>
2021-05-03 08:09:22 +0900

    #112 productsモデルのjson変換設定と一覧取得メソッドを作成

--- go_api/app/models/product.go
+++ go_api/app/models/product.go
@@ -1,12 +1,43 @@
 package models
 
-import "time"
+import (
+	"log"
+	"time"
+)
 
 type Product struct {
-	ID   int
-	UUID string
-	Name string
-	Description string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Id          int
+	Uuid        string    `json:"uuid,omitempty"`
+	Name        string    `json:"name,omitempty"`
+	Description string    `json:"description,omitempty"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type Products []Product
+
+func GetProducts() (products Products, err error) {
+	cmd := `SELECT id, uuid, name, description, created_at, updated_at
+			FROM product`
+	rows, err := Db.Query(cmd)
+	if err != nil {
+		log.Println(err)
+	}
+	for rows.Next() {
+		product := Product{}
+		err = rows.Scan(
+			&product.Id,
+			&product.Uuid,
+			&product.Name,
+			&product.Description,
+			&product.CreatedAt,
+			&product.UpdatedAt,
+		)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		products = append(products, product)
+	}
+	rows.Close()
+	return products, err
 }
