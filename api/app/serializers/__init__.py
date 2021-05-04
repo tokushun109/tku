ab0c5b35cd0193a5a0f836d6fdbe841180ecf572
tokunaga <tokunaga@limar.co.jp>
2021-05-04 12:21:48 +0900

    #122 apiをpythonからgoに変更

--- api/app/serializers/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.serializers.category import AccessoryCategorySerializer, MaterialCategorySerializer
-from app.serializers.site import SalesSiteSerializer, SkillMarketSerializer, SnsSerializer
-from app.serializers.product import ProductImageSerializer, ProductSerializer
-from app.serializers.producer_profile import ProducerProfileSerializer
--- api/app/serializers/producer_profile.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from app.models.producer_profile import ProducerProfile
-
-
-class ProducerProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProducerProfile
-        fields = ('name', 'introduction', 'logo')
