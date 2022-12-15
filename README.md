6f170dc6a0564971ba371f90fc9e7333ba865b72
tokushun109 <tokushun109@gmail.com>
2022-12-15 16:02:05 +0900

    #470 noindexの削除

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # tkuのハンドメイド販売サイトです
 
+## URL
+
+<https://tocoriri.com>
+
 ## インフラ構成図(予定)
 
 ![インフラ構成図](./infra/diagram.png)
--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -20,11 +20,6 @@ module.exports = {
                 name: 'description',
                 content: 'tocoriri web site',
             },
-            // 本番稼働させたら記述を変更する
-            {
-                name: 'robots',
-                content: 'noindex'
-            }
         ],
         link: [
             {
