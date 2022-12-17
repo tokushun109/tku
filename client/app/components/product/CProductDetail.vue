2f65a03d46557e103c40fcb291c58dcbf24433ff
tokushun109 <tokushun109@gmail.com>
2022-12-17 14:12:01 +0900

    #457 商品詳細ページの修正

--- client/app/components/product/CProductDetail.vue
+++ client/app/components/product/CProductDetail.vue
@@ -5,7 +5,7 @@
                 <p class="product-name">{{ product.name }}</p>
                 <v-row class="detail-area">
                     <v-col cols="12" sm="6">
-                        <c-product-image class="mb-4" category :product="product" />
+                        <c-product-image class="mb-4" :show-arrows="false" list-display :product="product" />
                     </v-col>
                     <v-col cols="12" sm="6">
                         <div class="price-area">
@@ -17,18 +17,29 @@
                         <div class="description-area">
                             <pre class="description text-body-2">{{ product.description }}</pre>
                         </div>
-                        <div class="tag-area">
-                            <p v-if="product.tags.length > 0" class="tag">
+                        <div v-if="product.category.name" class="category-area">
+                            <p class="category">
+                                カテゴリー<client-only><v-divider /></client-only>
+                            </p>
+
+                            <div class="category-content">
+                                <v-chip small :color="ColorType.LightGreen" :text-color="ColorType.White" class="mx-1">{{
+                                    product.category.name
+                                }}</v-chip>
+                            </div>
+                        </div>
+                        <div v-if="product.tags.length > 0" class="tag-area">
+                            <p v class="tag">
                                 関連タグ<client-only><v-divider /></client-only>
                             </p>
 
                             <div class="tag-content">
                                 <div v-for="tag in product.tags" :key="tag.uuid">
-                                    <v-chip small :color="ColorType.Lime" :text-color="ColorType.White" class="mx-1">{{ tag.name }}</v-chip>
+                                    <v-chip small :color="ColorType.LightGreen" :text-color="ColorType.White" class="mx-1">{{ tag.name }}</v-chip>
                                 </div>
                             </div>
                         </div>
-                        <div class="sales-site-area">
+                        <div v-if="product.siteDetails.length > 0" class="sales-site-area">
                             <div class="sales-site text-body-1">
                                 <p>
                                     販売サイト<client-only><v-divider /></client-only>
@@ -37,7 +48,7 @@
                             <v-row>
                                 <v-col v-for="siteDetail in product.siteDetails" :key="siteDetail.uuid" md="6" sm="12">
                                     <v-btn
-                                        :color="ColorType.LightGreen"
+                                        :color="ColorType.Orange"
                                         :href="siteDetail.detailUrl"
                                         class="site-buttons"
                                         target="_blank"
@@ -53,21 +64,23 @@
                 </v-row>
             </v-sheet>
         </v-container>
-        <div class="purchase-button">
-            <v-btn :color="ColorType.Orange" :disabled="product.siteDetails.length === 0" x-large fab @click="dialogVisible = true">
-                <c-icon :type="IconType.Cart.name" :color="ColorType.White" @c-click="dialogVisible = true" />
-            </v-btn>
-        </div>
+        <client-only>
+            <div class="purchase-button">
+                <v-btn :color="ColorType.Orange" :disabled="product.siteDetails.length === 0" x-large fab @click="dialogVisible = true">
+                    <c-icon :type="IconType.Cart.name" :color="ColorType.White" @c-click="dialogVisible = true" />
+                </v-btn>
+            </div>
+        </client-only>
         <c-dialog class="sales-site-dialog" :visible.sync="dialogVisible" :is-button="false">
             <template #content>
                 <div class="message-area">
                     <p class="message">以下のサイトで販売中です！<v-divider /></p>
                 </div>
-                <c-container>
+                <v-container>
                     <v-row>
                         <v-col v-for="siteDetail in product.siteDetails" :key="siteDetail.uuid" cols="12" md="6">
                             <v-btn
-                                :color="ColorType.LightGreen"
+                                :color="ColorType.Orange"
                                 :href="siteDetail.detailUrl"
                                 class="white--text text-h6 site-modal"
                                 target="_blank"
@@ -78,7 +91,7 @@
                             </v-btn>
                         </v-col>
                     </v-row>
-                </c-container>
+                </v-container>
             </template>
         </c-dialog>
     </v-sheet>
@@ -101,6 +114,8 @@ export default class CProductDetail extends Vue {
 <style lang="stylus" scoped>
 .c-product-detail-page
     position relative
+    +sm()
+        padding-bottom 40px
     .c-product-detail-page-wrapper
         .c-product-detail-page-area
             padding 16px
@@ -116,8 +131,14 @@ export default class CProductDetail extends Vue {
                     .description
                         margin-bottom 16px
                         white-space pre-wrap
+                .category-area
+                    .category
+                        font-weight $title-font-weight
+                    .category-content
+                        display flex
                 .tag-area
                     .tag
+                        margin-top 16px
                         font-weight $title-font-weight
                     .tag-content
                         display flex
--- client/app/components/product/CProductImage.vue
+++ client/app/components/product/CProductImage.vue
@@ -3,35 +3,56 @@
         <v-chip v-if="category && product.category.uuid" class="category-label" small :color="ColorType.LightGreen" :text-color="ColorType.White">{{
             product.category.name
         }}</v-chip>
-        <v-carousel
-            v-if="product.productImages.length > 0"
-            :show-arrows="product.productImages.length > 1"
-            height="auto"
-            hide-delimiters
-            class="carousel-area"
-        >
-            <template v-if="product.productImages.length > 1" #prev="{ on, attrs }">
-                <v-icon large v-bind="attrs" v-on="on">{{ mdiChevronLeft }}</v-icon>
-            </template>
-            <template v-if="product.productImages.length > 1" #next="{ on, attrs }">
-                <v-icon large v-bind="attrs" v-on="on">{{ mdiChevronRight }}</v-icon>
-            </template>
-            <v-carousel-item v-for="image in product.productImages" :key="image.uuid">
-                <v-img :src="image.apiPath" :alt="image.uuid" class="carousel-image" />
-            </v-carousel-item>
-        </v-carousel>
-        <v-carousel v-else :show-arrows="false" height="auto" hide-delimiters class="carousel-area">
-            <v-carousel-item>
-                <v-img src="/img/product/no-image.png" alt="no-image" class="carousel-image" />
-            </v-carousel-item>
-        </v-carousel>
+        <template v-if="!listDisplay">
+            <v-carousel
+                v-if="product.productImages.length > 0"
+                :show-arrows="showArrows && product.productImages.length > 1"
+                height="auto"
+                hide-delimiters
+                class="carousel-area"
+            >
+                <template v-if="product.productImages.length > 1" #prev="{ on, attrs }">
+                    <v-icon large v-bind="attrs" v-on="on">{{ mdiChevronLeft }}</v-icon>
+                </template>
+                <template v-if="product.productImages.length > 1" #next="{ on, attrs }">
+                    <v-icon large v-bind="attrs" v-on="on">{{ mdiChevronRight }}</v-icon>
+                </template>
+                <v-carousel-item v-for="image in product.productImages" :key="image.uuid">
+                    <v-img :src="image.apiPath" :alt="image.uuid" class="carousel-image" />
+                </v-carousel-item>
+            </v-carousel>
+            <v-carousel v-else :show-arrows="false" height="auto" hide-delimiters class="carousel-area">
+                <v-carousel-item>
+                    <v-img src="/img/product/no-image.png" alt="no-image" class="carousel-image" />
+                </v-carousel-item>
+            </v-carousel>
+        </template>
+        <template v-else>
+            <v-list>
+                <div class="display-area">
+                    <v-img v-if="product.productImages.length > 1" :src="selectImage.apiPath" :alt="selectImage.uuid" class="carousel-image" />
+                    <v-img v-else src="/img/product/no-image.png" alt="no-image" class="carousel-image" />
+                </div>
+                <v-row dense>
+                    <v-col v-for="(productImage, index) in product.productImages" :key="productImage.uuid" cols="2">
+                        <v-img
+                            :src="productImage.apiPath"
+                            :alt="productImage.uuid"
+                            class="carousel-image list-display"
+                            :class="{ 'is-select': isSelect(index) }"
+                            @click="setSelectIndex(index)"
+                        />
+                    </v-col>
+                </v-row>
+            </v-list>
+        </template>
     </v-sheet>
 </template>
 
 <script lang="ts">
 import { mdiChevronLeft, mdiChevronRight } from '@mdi/js'
 import { Component, Vue, Prop } from 'nuxt-property-decorator'
-import { ColorType, IProduct } from '~/types'
+import { ColorType, IProduct, IProductImage } from '~/types'
 
 @Component({})
 export default class CProductImage extends Vue {
@@ -44,6 +65,24 @@ export default class CProductImage extends Vue {
     @Prop({ type: Boolean, default: false }) title!: boolean
     // カテゴリーを画像上に表示する
     @Prop({ type: Boolean, default: false }) category!: boolean
+    // スライド用の矢印を表示する
+    @Prop({ type: Boolean, default: true }) showArrows!: boolean
+    // 登録画像をリスト形式で表示
+    @Prop({ type: Boolean, default: false }) listDisplay!: boolean
+
+    selectIndex: number = 0
+
+    setSelectIndex(index: number) {
+        this.selectIndex = index
+    }
+
+    isSelect(index: number): boolean {
+        return this.selectIndex === index
+    }
+
+    get selectImage(): IProductImage {
+        return this.product.productImages[this.selectIndex]
+    }
 }
 </script>
 
@@ -63,4 +102,11 @@ export default class CProductImage extends Vue {
         width 100%
         aspect-ratio 1 / 1
         object-fit cover
+        &.list-display
+            border solid 0.5px
+        &.is-select
+            border solid red 2px
+    .display-area
+        margin-bottom 8px
+        border-radius $image-border-radius
 </style>
