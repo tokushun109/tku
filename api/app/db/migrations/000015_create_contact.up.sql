c5704068d5be406d260104f685975003a8942bc2
tokushun109 <tokushun109@gmail.com>
2022-12-19 10:06:03 +0900

    #282 contactテーブルの作成

---
+++ api/app/db/migrations/000015_create_contact.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS contact(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    company VARCHAR(30),
+    phone_number VARCHAR(30),
+    mail_address VARCHAR(50) NOT NULL,
+    content TEXT(1000) NOT NULL,
+    is_read BOOLEAN NOT NULL DEFAULT 0,
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME
+);
