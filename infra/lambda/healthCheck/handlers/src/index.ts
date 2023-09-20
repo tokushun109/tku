e068aab537bcfaa07ec288da0a7cbf3e8cd1c6d2
tokushun109 <tokushun109@gmail.com>
2023-09-20 19:26:09 +0900

    #617 ヘルスチェック確認用のlambdaを作成

--- infra/lambda/healthCheck/handlers/src/index.ts
+++ infra/lambda/healthCheck/handlers/src/index.ts
@@ -1,9 +1,32 @@
+import fetch from 'node-fetch'
+
 export const handler = async () => {
-    return {
-        statusCode: 200,
-        body: {
-            message: 'バリデーションOK',
-            params: 'test',
-        },
+
+    try {
+        const res = await fetch('https://api.tocoriri.com/api/health_check', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: 'GET',
+        })
+        const result = await res.json() as { success?: boolean }
+        if (result.success === undefined) {
+            throw new Error('api取得失敗')
+        }
+        return {
+            statusCode: 200,
+            body: {
+                message: 'api取得OK',
+                result
+            },
+        }
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: {
+                message: 'apiの取得に失敗しました',
+                result: null
+            },
+        }
     }
 }
