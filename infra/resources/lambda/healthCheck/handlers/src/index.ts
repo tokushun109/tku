06324f8758fbb02d4a69ea69f6b5b90838161e37
tokushun109 <tokushun109@gmail.com>
2025-02-24 05:33:48 +0900

    #657 health checkをline messaging apiに変更

--- infra/resources/lambda/healthCheck/handlers/src/index.ts
+++ infra/resources/lambda/healthCheck/handlers/src/index.ts
@@ -4,16 +4,15 @@ export const handler = async () => {
     const timeout = setTimeout(async () => {
         await lineNotification()
         throw new Error('タイムアウトしました')
-    }, 15000);
+    }, 15000)
     try {
-
         const res = await fetch('https://api.tocoriri.com/api/health_check', {
             headers: {
                 'Content-Type': 'application/json',
             },
             method: 'GET',
         })
-        const result = await res.json() as { success?: boolean }
+        const result = (await res.json()) as { success?: boolean }
         if (result.success === undefined) {
             throw new Error('api取得失敗')
         }
@@ -22,7 +21,7 @@ export const handler = async () => {
             statusCode: 200,
             body: {
                 message: 'api取得OK',
-                result
+                result,
             },
         }
     } catch (e) {
@@ -31,23 +30,36 @@ export const handler = async () => {
             statusCode: 500,
             body: {
                 message: 'apiの取得に失敗しました',
-                result: null
+                result: null,
             },
         }
-    } finally { clearTimeout(timeout); }
+    } finally {
+        clearTimeout(timeout)
+    }
 }
 
 const lineNotification = async () => {
-    const qs = require('querystring')
-    await fetch('https://notify-api.line.me/api/notify', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Authorization: `Bearer ${process.env.LINE_HEALTH_CHECK_TOKEN}`,
-        },
-        body: qs.stringify({
-            message: 'とこりりのヘルスチェックに失敗しました。サーバーが落ちている可能性があります。',
-        }),
-    })
-}
+    const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.LINE_HEALTH_CHECK_TOKEN}`,
+    }
 
+    const payload = {
+        to: process.env.LINE_HEALTH_CHECK_USER_ID,
+        messages: [
+            {
+                type: 'text',
+                text: 'とこりりのヘルスチェックに失敗しました。サーバーが落ちている可能性があります。',
+            },
+        ],
+    }
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: headers,
+    }
+
+    await fetch('https://api.line.me/v2/bot/message/push', options)
+    console.log('送信しました')
+}
