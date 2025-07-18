8ed3458de109b929a66dc1e3e23f02dc59785a61
tokushun109 <tokushun109@gmail.com>
2025-07-18 14:07:47 +0900

    #694 adminページ用のlayout.tsxを作成

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -3,9 +3,11 @@
 import AdminFooter from '@/components/layouts/AdminFooter'
 import AdminHeader from '@/components/layouts/AdminHeader'
 
+import styles from './layout.module.scss'
+
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div>
+        <div className={styles['container']}>
             <AdminHeader />
             <main>{children}</main>
             <AdminFooter />
