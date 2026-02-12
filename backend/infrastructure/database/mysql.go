76805237b1686ede32f35732642cc779de75739e
tokushun109 <tokushun109@gmail.com>
2026-02-12 11:50:56 +0900

    #871 mysqlの接続設定の見直し

--- backend/infrastructure/database/mysql.go
+++ backend/infrastructure/database/mysql.go
@@ -2,11 +2,11 @@ package database
 
 import (
 	"errors"
-	"fmt"
 	"os"
 	"sync"
 	"time"
 
+	mysqlDriver "github.com/go-sql-driver/mysql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
@@ -34,14 +34,22 @@ func newMySQLFromEnv() (*gorm.DB, error) {
 		return nil, errors.New("DB_USER, DB_PASS, DB_NAME, and MYSQL_HOST are required")
 	}
 
-	protocol := fmt.Sprintf("tcp(%s)", host)
-	dsn := fmt.Sprintf(
-		"%s:%s@%s/%s?charset=utf8mb4&parseTime=True&loc=Asia%%2FTokyo&timeout=5s&readTimeout=5s&writeTimeout=5s",
-		user,
-		pass,
-		protocol,
-		name,
-	)
+	cfg := mysqlDriver.NewConfig()
+	cfg.User = user
+	cfg.Passwd = pass
+	cfg.Net = "tcp"
+	cfg.Addr = host
+	cfg.DBName = name
+	cfg.Params = map[string]string{
+		"charset":      "utf8mb4",
+		"parseTime":    "True",
+		"loc":          "Asia/Tokyo",
+		"timeout":      "5s",
+		"readTimeout":  "5s",
+		"writeTimeout": "5s",
+	}
+
+	dsn := cfg.FormatDSN()
 
 	gdb, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{SingularTable: true},
