871e14bfa22e121cd2c6ae16546d2f60ca524b5d
tokushun109 <tokushun109@gmail.com>
2026-02-15 10:12:30 +0900

    #877 shellコマンドの修正

--- clean-backend/docker/migrations/entrypoint.sh
+++ clean-backend/docker/migrations/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-migrate -path /migrations -database "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST})/${DB_NAME}" $@
+migrate -path /migrations -database "mysql://${DB_USER}:${DB_PASS}@tcp(${MYSQL_HOST}:${MYSQL_PORT})/${DB_NAME}" $@
